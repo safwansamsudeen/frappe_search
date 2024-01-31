c9628e6c8c3841cab5bae85694950a44ff31f56c
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-01-31 13:08:05 +0530

    feat: improved index, show results

--- frappe_search/frappe_search/doctype/search/search.js
+++ frappe_search/frappe_search/doctype/search/search.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, Safwan Samsudeen and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Search", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("DocType", {
+  after_save(frm) {
+    frappe.call({
+      method:
+        "frappe_search.frappe_search.doctype.search.search.tantivy_search",
+      args: { query_txt: frm.doc.query },
+      callback: (e) => {
+        let groups = Object.groupBy(e.message, (r) => r.doctype);
+        let sortedGroups = Object.entries(groups).sort(
+          (l, l2) => l2[1].length - l[1].length
+        );
+        let html = "";
+        for (let [groupName, results] of sortedGroups) {
+          html += `<div class="py-3"> <h3>${groupName}</h3>`;
+          html += `${results.map((l) => l.url).join("<br />")}</div>`;
+        }
+        frappe.msgprint(html || "No Results", "Search Results");
+      },
+    });
+  },
+});
--- frappe_search/frappe_search/doctype/search/search.py
+++ frappe_search/frappe_search/doctype/search/search.py
@@ -1,56 +1,85 @@
 # Copyright (c) 2024, Safwan Samsudeen and contributors
 # For license information, please see license.txt
 
-import frappe
 from tantivy import Document, Index, SchemaBuilder
+
+import frappe
+from frappe.core.utils import html2text
+from frappe.utils.data import get_absolute_url
 
 
 INDEX_PATH = "/Users/safwan/frappe-bench/apps/frappe_search/index"
+EXCLUDED_DOCTYPES = [
+    "DocField",
+    "Workspace Shortcut",
+    "Activity Log",
+    "Notification Settings",
+]
 
-@frappe.whitelist()
-def create_index():
-    global index
-    DOCTYPES = {
-        "Activity Log": {
-            "title": "subject",
-            "content": "ip_address",  
-		},
-        "User": {"title": "full_name", "content": "email"},
-    }
-    
-    schema = get_schema()
-    index = Index(schema, path=INDEX_PATH)
-    writer = index.writer()
-    
-    docs = []
-    
-    for doctype, fields in DOCTYPES.items():
-        for d in frappe.db.get_all(
-            doctype,
-            fields=list(fields.values()),
-        ):
-            docs.append({**{field: d[field_name] for field, field_name in fields.items()}, "doctype": doctype})
-            writer.add_document(Document(
-                **{field: d[field_name] for field, field_name in fields.items()}, doctype=doctype))
-    writer.commit()
-
-    return docs
 
 @frappe.whitelist()
 def tantivy_search(query_txt):
     index = Index.open(INDEX_PATH)
     searcher = index.searcher()
-    schema = get_schema()
-    query = index.parse_query(query_txt, ["title", "content", "doctype"])
+    query = index.parse_query(query_txt, ["title", "content", "name"])
     return [
-        searcher.doc(best_doc_address).to_dict()
-        for _, best_doc_address in searcher.search(query, 3).hits
+        {**(r := searcher.doc(best_doc_address).to_dict()), "url": format_result(r)}
+        for _, best_doc_address in searcher.search(query, 30).hits
     ]
+
+
+def format_result(record):
+    return f'<a href="{get_absolute_url(record["doctype"][0], record["name"][0])}">{record["title"][0]}</a>'
+
 
 def get_schema():
     schema_builder = SchemaBuilder()
     schema_builder.add_text_field("title", stored=True)
     schema_builder.add_text_field("content", stored=True)
     schema_builder.add_text_field("doctype", stored=True)
+    schema_builder.add_text_field("name", stored=True)
     return schema_builder.build()
 
+
+@frappe.whitelist()
+def complete_index():
+    doctypes = frappe.get_all(
+        "DocType",
+        fields=["name", "title_field"],
+    )
+    schema = get_schema()
+    index = Index(schema, path=INDEX_PATH)
+    writer = index.writer()
+
+    records = []
+    no_records = 0
+
+    for doctype in doctypes:
+        if doctype["name"] in EXCLUDED_DOCTYPES:
+            continue
+        doctype_obj = frappe.get_doc("DocType", doctype["name"])
+        content_fields = [
+            field.fieldname for field in doctype_obj.fields if field.in_global_search
+        ]
+        if doctype_obj.index_web_pages_for_search and not doctype_obj.issingle:
+            title_field = doctype["title_field"] or "name"
+            db_records = frappe.get_all(
+                doctype["name"], fields=[title_field, *content_fields, "name"]
+            )
+            if db_records:
+                for record in db_records:
+                    title = record.pop(title_field)
+                    data = {
+                        "title": title,
+                        "content": "\n".join(
+                            map(lambda x: html2text(str(x)), record.values())
+                        ),
+                        "name": record.name or title,
+                        "doctype": doctype["name"],
+                    }
+                    no_records += 1
+                    records.append(data)
+                    writer.add_document(Document(**data))
+
+    writer.commit()
+    return records, no_records
